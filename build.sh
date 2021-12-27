622a3e1476c00eb90f308909ec5e3e48ff8db9ec
shurabh <shurabh@gmail.com>
2021-12-27 10:07:13 +0530

    deleted few lines

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 #testing image version can be used from jenkins pipeline
 #echo see $(nslookup $1)
-echo see $(docker run --rm shurabh/utility:$1 nslookup gmail.com)
+echo $(docker run --rm shurabh/utility:$1 nslookup gmail.com)
