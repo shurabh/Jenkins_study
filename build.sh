bd72b420ccd0aef535fe152d4387ab5046a4706f
shurabh <shurabh@gmail.com>
2021-12-27 12:01:15 +0530

    echo

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #testing image version can be used from jenkins pipeline
 #echo see $(nslookup $1)
-echo $(docker run --rm shurabh/utility:$1 nslookup gmail.com)
+#echo $(docker run --rm shurabh/utility:$1 nslookup gmail.com)
+echo $1
